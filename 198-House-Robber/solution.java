a198d3f8965cb31259bae289dad40e693bf340ee
bumbing <mingjian.liu1@gmail.com>
2016-06-21 22:48:44 -0700

    [198. House Robber][Accepted]committed by LeetCode Extension

---
+++ 198-House-Robber/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int ro1(int[] nums){
+        //Space O(1), Time O(n)
+        if(nums.length == 0)    return 0;
+        if(nums.length == 1)    return nums[0];
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        deque.add(nums[0]);
+        deque.add(Math.max(nums[1], nums[0]));
+        for(int i=2; i<nums.length; i++){
+            deque.add(Math.max(deque.peekLast(), deque.pollFirst()+nums[i]));
+        }
+        return deque.peekLast();
+    }
+    
+    
+    public int rob(int[] nums) {
+        if(nums.length == 0)    return 0;
+        if(nums.length == 1)    return nums[0];
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        result[1] = Math.max(nums[1], nums[0]);
+        for(int i=2; i<nums.length; i++){
+            result[i] = Math.max(result[i-1], result[i-2]+nums[i]);
+        }
+        return result[nums.length-1];
+        //Space O(n), Time O(n);
+    }
+}
