2501b2a9c0f4782a005aebedb9807d90803eb42f
bumbing <mingjian.liu1@gmail.com>
2016-06-22 20:09:18 -0700

    [53. Maximum Subarray][Accepted]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,9 +1,7 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
         if(nums.length==0)  return 0;
-        int sum = 0;
-        int min = 0;
-        int result = nums[0];
+        int sum = 0, min = 0, result = nums[0];
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
             result = Math.max(result, sum-min);
