731f0ffff1d2f5c7b9ddb8b930818e84b191cc7f
bumbing <mingjian.liu1@gmail.com>
2016-06-23 16:26:18 -0700

    [279. Perfect Squares][Compile Error]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public int numSquares(int n) {
-        int[] result = new int[n.length];
-        for(int i=1; i<=n.length; i++){
+        int[] result = new int[n + 1];
+        for(int i=1; i<=n; i++){
             int tmp = Math.sqrt(n);
             if(n == tmp * tmp){
                 result[i] = 1;
