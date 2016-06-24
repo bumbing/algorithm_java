12a8ee655f8484bb320faf1ba236eaa34eebfd8d
bumbing <mingjian.liu1@gmail.com>
2016-06-23 17:40:49 -0700

    [84. Largest Rectangle in Histogram][Time Limit Exceeded]committed by LeetCode Extension

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -5,8 +5,14 @@ public class Solution {
         ArrayList<Integer> hist = new ArrayList<>();
         for(int i=0; i<heights.length; i++){
             while(!hist.isEmpty() && hist.get(hist.size()-1)>heights[i]){
-                result = Math.max(result, heights[hist.get(hist.size()-1)] * (i - hist.size()==1?-1:hist.get(hist.size()-2))
+                result = Math.max(result, heights[hist.get(hist.size()-1)] * (i - hist.size()==1?-1:hist.get(hist.size()-2)));
+                hist.remove(hist.size()-1);
             }
+            if(hist.isEmpty() || hist.get(hist.size()-1)<heights[i])   hist.add(i);
+        }
+        hist.add(0, -1);
+        while(hist.size()>1){
+            result = Math.max(result, heights[hist.get(hist.size()-1)] * (heights.length - 1 - hist.get(hist.size()-2)));
         }
         return result;
     }
