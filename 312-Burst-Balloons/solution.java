8924d7c31d2d92f2288a49263accdbcaa146434e
bumbing <mingjian.liu1@gmail.com>
2016-06-24 20:09:11 -0700

    [312. Burst Balloons][Accepted]committed by LeetCode Extension

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -7,10 +7,7 @@ public class Solution {
                 int head = tail - length;
                 int product = ((head!=0)?nums[head - 1]:1) * ((tail != nums.length-1)?nums[tail+1]:1);
                 for(int i = head; i <= tail; i++){
-                    int coins = 0;
-                    if(i - 1 >= head)   coins += result[i-1-head][i-1];
-                    if(i + 1 <= tail)   coins += result[tail-i-1][tail];
-                    coins += product * nums[i];
+                    int coins = product * nums[i] + ((i>head)?result[i-1-head][i-1]:0) + ((i<tail)?result[tail-i-1][tail]:0);
                     result[length][tail] = Math.max(result[length][tail], coins);
                 }
             }
