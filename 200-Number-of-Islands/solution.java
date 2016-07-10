7f8ad7907fdf2b7ba39d81ce782685c92895004d
bumbing <mingjian.liu1@gmail.com>
2016-07-10 11:52:53 -0400

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,51 +1,64 @@
 public class Solution {
     public int numIslands(char[][] grid) {
+        //union find
         if(grid.length==0)  return 0;
         if(grid[0].length==0)   return 0;
         int result = 0;
-        //dfs
-        Stack<Integer> stack = new Stack();
-        boolean[][] traversed = new boolean[grid.length][grid[0].length];
+        int[][] root = new int[grid.length][grid[0].length];
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++)
+                root[i][j] = i*grid[0].length + j;
+        }
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j]=='1'&&traversed[i][j]==false){
-                    traversed[i][j] = true;
-                    result++;
-                    stack.push(i*grid[0].length+j);
-                    traversed[i][j] = true;
-                    while(!stack.empty()){
-                        int temp = stack.pop();
-                        int k = temp/grid[0].length;
-                        int l = temp%grid[0].length;
-                        if(k>0){
-                            if(grid[k-1][l]=='1' && traversed[k-1][l]==false){
-                                stack.push(temp-grid[0].length);
-                                traversed[k-1][l]=true;
+                if(grid[i][j]=='1'){
+                    if(i>0 && j>0){
+                        if(grid[i-1][j]=='1'&&grid[i][j-1]=='1'){
+                            root[i][j] = (i-1)*grid[0].length+j;
+                            if(!findroot(root, (i-1)*grid[0].length+j, i*grid[0].length+j-1)){
+                                result--;
+                                merge(root, (i-1)*grid[0].length+j, i*grid[0].length+j-1);
                             }
-                        }
-                        if(k<grid.length-1){
-                            if(grid[k+1][l]=='1' && traversed[k+1][l]==false){
-                                stack.push(temp+grid[0].length);
-                                traversed[k+1][l]=true;
-                            }
-                        }
-                        if(l>0){
-                            if(grid[k][l-1]=='1' && traversed[k][l-1]==false){
-                                stack.push(temp-1);
-                                traversed[k][l-1]=true;
-                            }
-                        }
-                        if(l<grid[0].length-1){
-                            if(grid[k][l+1]=='1' && traversed[k][l+1]==false){
-                                stack.push(temp+1);
-                                traversed[k][l+1]=true;
-                            }
-                        }
-                        
+                        }else if(grid[i-1][j]=='1'){
+                            root[i][j] = (i-1)*grid[0].length+j;
+                        }else if(grid[i][j-1]=='1'){
+                            root[i][j] = i*grid[0].length+j-1;
+                        }else   result++;
+                    }else if(i>0 && j==0){
+                        if(grid[i-1][j]=='1'){
+                            root[i][j] = (i-1)*grid[0].length+j;
+                        }else   result++;
+                    }else if(j>0 && i==0){
+                        if(grid[i][j-1]=='1'){
+                            root[i][j] = i*grid[0].length+j-1;
+                        }else   result++;
+                    }else{
+                        result++;
                     }
-                }     
+                }
             }
         }
         return result;
+    }
+    
+    private boolean findroot(int[][] root, int x, int y){
+        while(x!=root[x/root[0].length][x%root[0].length]){
+            x=root[x/root[0].length][x%root[0].length];
+        }
+        while(y!=root[y/root[0].length][y%root[0].length]){
+            y=root[y/root[0].length][y%root[0].length];
+        }
+        return x == y;
+    }
+    
+    private void merge(int[][] root, int x, int y){
+        while(x!=root[x/root[0].length][x%root[0].length]){
+            x=root[x/root[0].length][x%root[0].length];
+        }
+        while(y!=root[y/root[0].length][y%root[0].length]){
+            y=root[y/root[0].length][y%root[0].length];
+        }
+        root[y/root[0].length][y%root[0].length] = x;
+        return;
     }
 }
