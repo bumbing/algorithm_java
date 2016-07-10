6c52d96b58ab0dd8790e602484795d1183d0679b
bumbing <mingjian.liu1@gmail.com>
2016-07-10 11:07:39 -0400

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -3,43 +3,49 @@ public class Solution {
         if(grid.length==0)  return 0;
         if(grid[0].length==0)   return 0;
         int result = 0;
-        //bfs
+        //dfs
+        Stack<Integer> stack = new Stack();
         boolean[][] traversed = new boolean[grid.length][grid[0].length];
-        for(boolean[] group: traversed)     Arrays.fill(group, false);
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                if(!traversed[i][j] && grid[i][j]=='1'){
-                    result++;
-                    ArrayList<Integer> nodes = new ArrayList<Integer>();    
-                    nodes.add(i*grid[0].length + j);
+                if(grid[i][j]=='1'&&traversed[i][j]==false){
                     traversed[i][j] = true;
-                    while(!nodes.isEmpty()){
-                        int temp = nodes.get(0);
-                        nodes.remove(0);
+                    result++;
+                    stack.push(i*grid[0].length+j);
+                    traversed[i][j] = true;
+                    while(!stack.empty()){
+                        int temp = stack.pop();
                         int k = temp/grid[0].length;
                         int l = temp%grid[0].length;
                         if(k>0){
-                            if(!traversed[k-1][l] && grid[k-1][l]=='1') nodes.add((k-1)*grid[0].length + l);
-                            traversed[k-1][l] = true;
+                            if(grid[k-1][l]=='1' && traversed[k-1][l]==false){
+                                stack.push(temp-grid[0].length);
+                                traversed[k-1][l]=true;
+                            }
                         }
                         if(k<grid.length-1){
-                            if(!traversed[k+1][l] && grid[k+1][l]=='1') nodes.add((k+1)*grid[0].length + l);
-                            traversed[k+1][l] = true;
+                            if(grid[k+1][l]=='1' && traversed[k+1][l]==false){
+                                stack.push(temp+grid[0].length);
+                                traversed[k+1][l]=true;
+                            }
                         }
                         if(l>0){
-                            if(!traversed[k][l-1] && grid[k][l-1]=='1') nodes.add(k*grid[0].length + l - 1);
-                            traversed[k][l-1] = true;
+                            if(grid[k][l-1]=='1' && traversed[k][l-1]==false){
+                                stack.push(temp-1);
+                                traversed[k][l-1]=true;
+                            }
                         }
                         if(l<grid[0].length-1){
-                            if(!traversed[k][l+1] && grid[k][l+1]=='1') nodes.add(k*grid[0].length + l + 1);
-                            traversed[k][l+1] = true;
+                            if(grid[k][l+1]=='1' && traversed[k][l+1]==false){
+                                stack.push(temp+1);
+                                traversed[k][l+1]=true;
+                            }
                         }
+                        
                     }
-                }
-                
+                }     
             }
         }
         return result;
-        
     }
 }
