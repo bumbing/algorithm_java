cd52555afa841e227bf07aa3b45c461203ecf285
bumbing <mingjian.liu1@gmail.com>
2016-07-20 01:34:57 -0400

    [98. Validate Binary Search Tree][Accepted]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -9,27 +9,14 @@
  */
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        int number = Integer.MIN_VALUE;
-        TreeNode cur = root;
-        Stack<TreeNode> stack = new Stack();
-        boolean first = false;
-        while(cur!=null){
-            stack.push(cur);
-            cur=cur.left;
-        }   
-        while(!stack.isEmpty()){
-            TreeNode temp = stack.pop();
-            if(temp.val <= number && first)  return false;
-            first = true;
-            number = temp.val;
-            temp = temp.right;
-            while(temp!=null){
-                stack.push(temp);
-                temp = temp.left;
-            }
-        }
-
-        return true;
+        if(root == null)    return true;
+        return helper(root, null, null);
     }
     
+    public boolean helper(TreeNode root, Integer min, Integer max){
+        if(root==null)  return true;
+        if(min!=null && root.val<=min)  return false;
+        if(max!=null && root.val>=max)  return false;
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
 }
