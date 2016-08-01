3e421806eb927bd25ca9461bc23852de40399ad6
bumbing <mingjian.liu1@gmail.com>
2016-07-31 20:50:37 -0700

    [74. Search a 2D Matrix][Accepted]committed by LeetCode Extension

---
+++ 74-Search-a-2D-Matrix/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0 || matrix[0].length==0) return false;
+        int head=0, tail=matrix.length-1;
+        while(head<tail-1){
+            
+        }
+    }
+}
