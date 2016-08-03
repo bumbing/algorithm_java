dd717c3085c224cb305e3cb74f66ac1353cf2ba6
bumbing <mingjian.liu1@gmail.com>
2016-08-03 03:14:16 -0700

    [115. Distinct Subsequences][Accepted]committed by LeetCode Extension

---
+++ 115-Distinct-Subsequences/solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int numDistinct(String s, String t) {
+        if(s.length()==0 || t.length()==0)  return 0;
+        int[][] result = new int[s.length()][t.length()];
+        for(int i=0; i<s.length(); i++){
+            for(int j=i; j<t.length(); j++){
+                
+            }
+        }
+    }
+}
