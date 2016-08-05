78358d36a1984e65de2daac16c0c6b59ca5866ea
bumbing <mingjian.liu1@gmail.com>
2016-08-05 12:11:23 -0700

    [303. Range Sum Query - Immutable][Accepted]committed by LeetCode Extension

---
+++ 303-Range-Sum-Query---Immutable/solution.java
@@ -0,0 +1,16 @@
+public class NumArray {
+
+    public NumArray(int[] nums) {
+        
+    }
+
+    public int sumRange(int i, int j) {
+        
+    }
+}
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
