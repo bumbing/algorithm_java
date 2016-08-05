a02db57f8514f2cff6cc301f963deb220e83f340
bumbing <mingjian.liu1@gmail.com>
2016-08-04 17:03:02 -0700

    [264. Ugly Number II][Accepted]committed by LeetCode Extension

---
+++ 264-Ugly-Number-II/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int nthUglyNumber(int n) {
+        Set<Long> traversed = new HashSet<>();
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        int[] array = {2, 3, 5};
+        pq.offer(1);
+        traversed.add(1l);
+        int count=0;
+        while(count<n){
+            count++;
+            int temp = pq.poll();
+            if(count==n)    return temp;
+            for(int num: array){
+                if(!traversed.contains(temp*num) && temp*num>0){
+                    traversed.add(temp*num);
+                    pq.offer(temp*num);
+                }
+            }     
+        }
+        return -1;
+    }
+}
