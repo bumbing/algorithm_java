363fa7d8cda18d1b22350f24caec472003db5d5f
bumbing <mingjian.liu1@gmail.com>
2016-08-06 11:25:29 -0700

    [259. 3Sum Smaller][Accepted]committed by LeetCode Extension

--- 259-3Sum-Smaller/solution.java
+++ 259-3Sum-Smaller/solution.java
@@ -1,23 +1,20 @@
 public class Solution {
-    int count;
-    
     public int threeSumSmaller(int[] nums, int target) {
-        count = 0;
         Arrays.sort(nums);
-        int len = nums.length;
-    
-        for(int i=0; i<len-2; i++) {
-            int left = i+1, right = len-1;
-            while(left < right) {
-                if(nums[i] + nums[left] + nums[right] < target) {
-                    count += right-left;
-                    left++;
-                } else {
-                    right--;
+        if(nums.length<3)   return 0;
+        int result = 0;
+        for(int i=0; i<nums.length-2; i++){
+            int head=i+1, tail=nums.length-1;
+            while(head<tail){
+                if(nums[i]+nums[head]+nums[tail]>=target){
+                    tail--;
+                }else{
+                    int tmp = nums[i]+nums[head]+nums[tail];
+                    result = Math.max(result, tmp);
+                    head++;
                 }
             }
         }
-        
-        return count;
+        return result;
     }
 }
