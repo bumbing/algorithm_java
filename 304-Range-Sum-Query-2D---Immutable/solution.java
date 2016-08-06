c1f3035c9821a00df113c087e9a8463e8ff75229
bumbing <mingjian.liu1@gmail.com>
2016-08-06 16:34:18 -0700

    [304. Range Sum Query 2D - Immutable][Accepted]committed by LeetCode Extension

---
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -0,0 +1,16 @@
+public class NumMatrix {
+
+    public NumMatrix(int[][] matrix) {
+        
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        
+    }
+}
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
