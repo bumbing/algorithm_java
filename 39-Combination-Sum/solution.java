b86e5ce8487e86104288fcb35501f09133315d3e
bumbing <mingjian.liu1@gmail.com>
2016-08-07 21:09:43 -0700

    [39. Combination Sum][Accepted]committed by LeetCode Extension

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -1,16 +1,23 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        return combinationHelper(candidates, target, 0);
+    }
+    public List<List<Integer>> combinationHelper(int[] candidates, int target, int index){
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for(int num: candidates){
+        for(int i=index; i<candidates.length; i++){
+            int num = candidates[i];
             List<List<Integer>> temp;
             if(target>num){
-                temp = combinationSum(candidates, target-num);
+                temp = combinationHelper(candidates, target-num, i);
                 for(List<Integer> lists: temp){
                     lists.add(num);
                     result.add(lists);
                 }
             }else if(target==num){
-                result.add(new ArrayList<Integer>().add(target));
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(target);
+                result.add(list);
             }
         }
         return result;
