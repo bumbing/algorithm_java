c79b7db6b7311cabcf712e89e75af9195e0e5e7c
bumbing <mingjian.liu1@gmail.com>
2016-08-07 20:34:16 -0700

    [135. Candy][Accepted]committed by LeetCode Extension

---
+++ 135-Candy/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        int[] left = new int[ratings.length], right = new int[ratings.length];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        for(int i=1; i<ratings.length; i++){
+            if(ratings[i] > ratings[i-1])   left[i] = left[i-1]+1;
+        }
+        for(int i=ratings.length-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1])   right[i] = right[i+1]+1;
+        }
+        int result = 0;
+        for(int i=0; i<ratings.length; i++){
+            result += Math.max(left[i], right[i]);
+        }
+        return result;
+    }
+}
