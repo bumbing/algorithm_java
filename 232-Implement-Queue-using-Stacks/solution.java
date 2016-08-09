099a9345871cb64f3d65cde53e48c81d2b12c79e
bumbing <mingjian.liu1@gmail.com>
2016-08-09 00:37:42 -0700

    [232. Implement Queue using Stacks][Accepted]committed by LeetCode Extension

--- 232-Implement-Queue-using-Stacks/solution.java
+++ 232-Implement-Queue-using-Stacks/solution.java
@@ -1,21 +1,32 @@
 class MyQueue {
     // Push element x to the back of queue.
+    Stack<Integer> s1 = new Stack(), s2 = new Stack();
+    int peek;
     public void push(int x) {
-        
+        //o(1)
+        if(s1.isEmpty())    peek = x;
+        s1.push(x);
     }
 
     // Removes the element from in front of queue.
     public void pop() {
-        
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        s2.pop();
+        while(!s2.isEmpty()){
+            push(s2.pop());
+        }
     }
 
     // Get the front element.
     public int peek() {
-        
+
+        return peek;
     }
 
     // Return whether the queue is empty.
     public boolean empty() {
-        
+        return s1.isEmpty();
     }
 }
