a277931f881a223dcc27774d38be6b833db4b538
bumbing <mingjian.liu1@gmail.com>
2016-08-11 16:22:49 -0700

    [344. Reverse String][Accepted]committed by LeetCode Extension

--- 344-Reverse-String/solution.java
+++ 344-Reverse-String/solution.java
@@ -1,5 +1,14 @@
 public class Solution {
     public String reverseString(String s) {
-        
+        char[] c = s.toCharArray();
+        int head =0, tail=c.length-1;
+        while(head<tail){
+            char tmp = c[head];
+            c[head] = c[tail];
+            c[tail] = tmp;
+            head++;
+            tail--;
+        }
+        return new String.valueOf(c);
     }
 }
