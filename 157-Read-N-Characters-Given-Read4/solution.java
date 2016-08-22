5438a5aea8c328683ff7b8aeeb07735278904e0b
bumbing <mingjian.liu1@gmail.com>
2016-08-22 14:22:43 -0700

    [157. Read N Characters Given Read4][Accepted]committed by LeetCode Extension

---
+++ 157-Read-N-Characters-Given-Read4/solution.java
@@ -0,0 +1,29 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        char[] temp = new char[4];
+        int count = 0, index = 0;
+        int leftChar = n;
+        boolean eof = false;
+        while(leftChar!=0){
+            if(count==0){
+                if(eof) break;
+                count = read4(temp);
+                if(count==0)    eof=true;
+            }else{
+                buf[index] = temp[4-count];
+                count--;
+                leftChar--;
+                index++;
+            }
+        }
+        return n-leftChar;
+    }
+}
