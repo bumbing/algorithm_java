9d49431e125234c08fae9a661929d33c2e656008
bumbing <mingjian.liu1@gmail.com>
2016-08-23 22:13:05 -0700

    [367. Valid Perfect Square][Accepted]committed by LeetCode Extension

--- 367-Valid-Perfect-Square/solution.java
+++ 367-Valid-Perfect-Square/solution.java
@@ -7,11 +7,11 @@ public class Solution {
     
     public int mySqrt(int x) {
         long head = 0, tail = 1<<16, mid;
-        while(head * head != x && tail * tail != x && head <= tail){
+        while(head * head != x && tail * tail != x && head+1 < tail){
             mid = (head + tail)/ 2;    
             if(mid * mid == x)   return (int)mid;
-            else if(mid * mid > x)   tail = mid -1;
-            else head = mid+1;
+            else if(mid * mid > x)   tail = mid;
+            else head = mid;
         }
         
         return (head * head == x)?(int)head:(int)tail;
