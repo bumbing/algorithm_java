515dbffdcbd7217d04969f5a2d6cde2015a198c4
bumbing <mingjian.liu1@gmail.com>
2016-08-27 12:15:10 -0700

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,5 +1,18 @@
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        for(int num: nums){
+            List<List<Integer>> templists = new ArrayList<List<Integer>>();
+            for(List<Integer> list: result){
+                for(int i=0; i<=list.size(); i++){
+                    List<Integer> templist = new ArrayList<Integer>(list);
+                    templist.add(i, num);
+                    templists.add(templist);
+                }
+            }
+            result = templists;
+        }
+        return result;
     }
 }
