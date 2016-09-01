dd5f3fb508ea47e59fc15722887ddd9c0de80ac5
bumbing <mingjian.liu1@gmail.com>
2016-09-01 15:15:15 -0700

    [251. Flatten 2D Vector][Accepted]committed by LeetCode Extension

---
+++ 251-Flatten-2D-Vector/solution.java
@@ -0,0 +1,22 @@
+public class Vector2D implements Iterator<Integer> {
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        
+    }
+
+    @Override
+    public Integer next() {
+        
+    }
+
+    @Override
+    public boolean hasNext() {
+        
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
