23dfbb39fba46b6c287575b3996aa6b3a59d97c0
bumbing <mingjian.liu1@gmail.com>
2016-09-03 20:21:45 -0700

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,15 +1,5 @@
 public class Solution {
     public int numDecodings(String s) {
-        if(s.length()==0) return s.length();
-        int x=1, y=(s.charAt(0)-'0'>0)?1:0;
-        for(int i=1; i<s.length(); i++){
-            int temp = 0;
-            if(Integer.parseInt(s.substring(i, i+1))>0)   temp += y;
-            int num = Integer.parseInt(s.substring(i-1, i+1));
-            if(num<=26 && num>=10)    temp += x;
-            x = y;
-            y = temp;
-        }
-        return y;
+        
     }
 }
