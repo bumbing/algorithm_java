da3d3bf4282141045f50f1e12ed2ce7c504c3a5c
bumbing <mingjian.liu1@gmail.com>
2016-09-05 15:19:52 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         if(s==null || p==null)  return false;
         boolean[][] match = new boolean[s.length()+1][p.length()+1];
         match[0][0] = true;
-        for (int i = 0; i < p.length(); i++) {
+        for (int i = 1; i < p.length(); i++) {
             if (p.charAt(i) == '*' && match[0][i-1]) {
                 match[0][i+1] = true;
             }
@@ -16,9 +16,9 @@ public class Solution {
                     match[i+1][j+1] = match[i][j];
                 }else if(p.charAt(j)=='*'){
                     if(p.charAt(j-1)==s.charAt(i)||p.charAt(j-1)=='.'){
-                        match[i+1][j+1] = match[i+1][j-1] || match[i][j-1] || match[i][j+1];
+                        match[i+1][j+1] = match[i+1][j-1] || match[i][j-1] || match[i][j+1];//nth, 1, multiple
                     }else{
-                        match[i+1][j+1] = match[i+1][j-1]; 
+                        match[i+1][j+1] = match[i+1][j-1]; //this is do nothing
                     }
                 }
             }
