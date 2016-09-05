fa5fe24330ac8d646121622fa10d3ceb72455151
bumbing <mingjian.liu1@gmail.com>
2016-09-05 15:17:52 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -3,13 +3,14 @@ public class Solution {
         if(s==null || p==null)  return false;
         boolean[][] match = new boolean[s.length()+1][p.length()+1];
         match[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && match[0][i-1]) {
+                match[0][i+1] = true;
+            }
+        }
 
         for(int i=0; i<s.length(); i++){
             for(int j=0; j<p.length(); j++){
-                if(i==0){
-                    if(p.charAt(j)=='*')
-                    match[i][j+1] = match[i][j-1];
-                }
                 
                 if(p.charAt(j)==s.charAt(i)||p.charAt(j)=='.'){
                     match[i+1][j+1] = match[i][j];
