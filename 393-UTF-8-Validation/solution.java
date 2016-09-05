89ec7b06d106800ae797018f3ac40d95f3ab7ff0
bumbing <mingjian.liu1@gmail.com>
2016-09-05 02:43:12 -0700

    [393. UTF-8 Validation][Accepted]committed by LeetCode Extension

---
+++ 393-UTF-8-Validation/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean validUtf8(int[] data) {
+        int head = 0;
+        while(head<data.length){
+            if(data[head]<128){
+                head++;
+            }else{
+                int tmp = data[head]-128;
+                int cnt = 0;
+                while(tmp>=64){
+                    tmp = 2 * (tmp - 64);
+                    cnt++;
+                }
+                if(cnt==7)  return false;
+                if(head+cnt+1>data.length)  return false;
+                head++;
+                while(cnt>0){
+                    if(data[head]>191 || data[head]<128)    return false;
+                    head++;
+                    cnt--;
+                }
+            }
+        }
+        return true;
+    }
+}
