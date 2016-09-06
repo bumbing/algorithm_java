1806293c7133ec5f9f16688f6e70b267219b26c2
bumbing <mingjian.liu1@gmail.com>
2016-09-05 21:53:34 -0700

    [84. Largest Rectangle in Histogram][Accepted]committed by LeetCode Extension

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -1,23 +1,31 @@
 public class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack();
         int result = 0;
+        Stack<Integer> nums = new Stack();
         for(int i=0; i<heights.length; i++){
-            result = Math.max(add(stack, i, heights[i], heights), result);
+            result = Math.max(result, addLast(nums, i, heights[i], heights));
         }
-        result = Math.max(add(stack, heights.length, 0, heights), result);
+        result = Math.max(result, addLast(nums, heights.length, 0, heights));
         return result;
     }
     
-    private int add(Stack<Integer> stack, int index, int height, int[] heights){
+    private int addLast(Stack<Integer> nums, int index, int height, int[] heights){
         int result = 0;
         while(true){
-            if(stack.empty())   break;
-            if(heights[stack.peek()] < height)   break;
-            int product = heights[stack.pop()] * (index - (stack.empty()?0:(stack.peek()+1)));
-            result = Math.max(product, result);
+            if(nums.isEmpty())  break;
+            int tmp = heights[nums.get(nums.size()-1)];
+            if(tmp < height) break;
+            if(tmp == height){
+                nums.remove(nums.size()-1);
+                break;
+            }
+            //calculate and remove the last one
+            int product = index;
+            product -= (nums.size()==1)?0:nums.get(nums.size()-2)+1; 
+            result = Math.max(result, tmp * product);
+            nums.remove(nums.size()-1);
         }
-        stack.push(index);
+        nums.add(index);
         return result;
     }
 }
