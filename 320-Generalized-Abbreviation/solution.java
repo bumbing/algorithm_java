055bc8398438ae8d5689258fa6291b6de4d1b854
bumbing <mingjian.liu1@gmail.com>
2016-09-13 21:57:50 -0700

    [320. Generalized Abbreviation][Accepted]committed by LeetCode Extension

--- 320-Generalized-Abbreviation/solution.java
+++ 320-Generalized-Abbreviation/solution.java
@@ -6,24 +6,16 @@ public class Solution {
     }
     
     private void dfs(List<String> result, char[] word, int index, int count, StringBuilder st){
-        //if index==word.length
         if(index==word.length){
             if(count!=0)    st.append(count);
             result.add(st.toString());
             return;
         }  
-        //else dfs
-        while(index<word.length){
-            StringBuilder temp = new StringBuilder(st);
-            if(count!=0){
-                dfs(result, word, index+1, count+1, new StringBuilder(st));
-                temp.append(count);
-            }else{
-                dfs(result, word, index+1, 1, new StringBuilder(st));
-            }
-            temp.append(word[index]);
-            dfs(result, word, index+1, 0, temp);
-        }
+        StringBuilder temp = new StringBuilder(st);
+        dfs(result, word, index+1, count+1, st);
+        if(count!=0)    temp.append(count);
+        temp.append(word[index]);
+        dfs(result, word, index+1, 0, temp);
         return;
     }
 }
